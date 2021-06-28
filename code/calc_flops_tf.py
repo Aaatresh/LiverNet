b719a86e16a794ac8c227a8a4121c54fe990a70f
aaatresh <aaa.171ec106@nitk.edu.in>
2021-06-28 18:42:32 +0530

    added flops calculate code

---
+++ code/calc_flops_tf.py
@@ -0,0 +1,24 @@
+def get_flops():
+    session = tf.compat.v1.Session()
+    graph = tf.compat.v1.get_default_graph()
+
+    with graph.as_default():
+        with session.as_default():
+            # with U.TimingManager('model load'):
+            model = create_model() ## initialize the model
+
+            run_meta = tf.compat.v1.RunMetadata()
+            opts = tf.compat.v1.profiler.ProfileOptionBuilder.float_operation()
+
+            # Optional: save printed results to file
+            # flops_log_path = os.path.join(tempfile.gettempdir(), 'tf_flops_log.txt')
+            # opts['output'] = 'file:outfile={}'.format(flops_log_path)
+
+            # We use the Keras session graph in the call to the profiler.
+            flops = tf.compat.v1.profiler.profile(graph=graph,
+                                                  run_meta=run_meta, cmd='op', options=opts)
+
+    tf.compat.v1.reset_default_graph()
+
+    return flops.total_float_ops
+
